b814f7ee1847211a54974c0075ea6c67aa0e94fb
Bruce Gilbert <webguync@gmail.com>
2019-02-03 18:19:14 -0500

    Updates

---
+++ precache-manifest.c5e6f62ada42815f42d1cff13f60a9b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "58820044b6cb07efb5b89794443721fe",
+    "url": "/react-spa/static/media/meow-kitty.58820044.jpg"
+  },
+  {
+    "revision": "c43f690f0277d25cac77c31291103a82",
+    "url": "/react-spa/static/media/doggoneit.c43f690f.jpg"
+  },
+  {
+    "revision": "395c3952b4a8a89da27ef31b81921c70",
+    "url": "/react-spa/static/media/cute-kitty.395c3952.jpg"
+  },
+  {
+    "revision": "6f3a4ee917403261f0909e88b0a7d585",
+    "url": "/react-spa/static/media/augie-doggie.6f3a4ee9.jpg"
+  },
+  {
+    "revision": "8ca1f4205a6302ffbcae",
+    "url": "/react-spa/static/js/runtime~main.8ca1f420.js"
+  },
+  {
+    "revision": "7ec7948003c2a1ec0ebb",
+    "url": "/react-spa/static/js/main.7ec79480.chunk.js"
+  },
+  {
+    "revision": "df0a3d546c43b1ddd39d",
+    "url": "/react-spa/static/js/1.df0a3d54.chunk.js"
+  },
+  {
+    "revision": "7ec7948003c2a1ec0ebb",
+    "url": "/react-spa/static/css/main.9805b5db.chunk.css"
+  },
+  {
+    "revision": "bf90cb9a7e17cc40f8a37f56b6af7eeb",
+    "url": "/react-spa/index.html"
+  }
+];
